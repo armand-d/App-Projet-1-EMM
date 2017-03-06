c105f0f0eb9309b8b6208697d9be67741f6e0a61
Armand <armand.fourtane@gmail.com>
2017-03-06 20:21:29 +0100

    chose icon Ctrl

--- www/js/controllers/ChoseIcon.js
+++ www/js/controllers/ChoseIcon.js
@@ -8,8 +8,36 @@
   function ChoseIconCtrl ($state) {
     const choseIcon = this;
 
-    choseIcon.mustaches = [1,2,3,4];
-    choseIcon.hats = [1,2,3,4];
+    // Data à mettre dans un autre fichier 
+    choseIcon.data = {
+      mustaches : {
+        title : 'Moustache',
+        data : {
+          mustache_1: {id: 1, url:'img/icons/mustache_1.svg'},
+          mustache_2: {id: 2,url:'img/icons/mustache_2.svg'},
+        }
+      },
+      hats : {
+        title : 'Chapeaux',
+        data : {
+          hat_1: {id: 1, url:'img/icons/mustache_1.svg'},
+          hat_2: {id: 2,url:'img/icons/mustache_2.svg'},
+        }
+      }
+    };
+    // fin data
+
+    choseIcon.change = item => {
+      if(choseIcon.confirmed[item]) {
+        console.log('add '+item);
+      } else {
+        console.log('remove '+item);
+      }
+    }
+
+    choseIcon.back = _ => {
+      $state.go('picValidate');
+    }
 
   };
 
